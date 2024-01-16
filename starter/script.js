98ef896a7012fa9a204f47b70f02605200e1e980
Kane <kane10@live.co.uk>
2024-01-16 19:16:01 +0000

    starter code and some JS

---
+++ starter/script.js
@@ -0,0 +1,48 @@
+var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+
+var now = dayjs().format(dddd-MMMM-D);
+
+console.log(now);
+
+
+var headerEl = document.getElementById('currentDay')
+
+
+.appendChild(now);
+
+
+
+
+// var myDay
+
+
+
+// console.log(dayjs());
+
+// const events = document.querySelectorAll(".event");
+// // console.log(events);
+
+// events.forEach( event => {
+//     console.log(event)
+// })
+
+
+
+
+
+
+
+
+
+// import dayjs from 'dayjs';#
+
+// var currentDay = document.getElementById("currentDay");
+
+// currentDay = dd/mm/yyyy;
+
+// const formatDate = (date) => {
+//     var day = date.getDate();
+//     return(day);
+// }
+
+// console.log(formatDate);
